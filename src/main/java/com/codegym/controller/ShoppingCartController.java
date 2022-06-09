73887e1b7497131eed5289168e6c5f4efbc6d678
unknown <phamthulien1975@gmail.com>
2022-06-09 14:30:24 +0700

    up

---
+++ src/main/java/com/codegym/controller/ShoppingCartController.java
@@ -0,0 +1,29 @@
+package com.codegym.controller;
+
+import com.codegym.model.Cart;
+import com.codegym.model.Product;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
+
+@Controller
+public class ShoppingCartController {
+
+    @ModelAttribute("cart")
+    public Cart setupCart(){
+        return new Cart();
+    }
+
+    @GetMapping("/shopping-cart")
+    public ModelAndView showCart (@SessionAttribute("cart") Cart cart){
+        ModelAndView modelAndView = new ModelAndView("/cart");
+        modelAndView.addObject("cart",cart);
+        return modelAndView;
+    }
+
+}
